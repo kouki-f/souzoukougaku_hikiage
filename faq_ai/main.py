17a41aea145299df3c093bf730f472b27ec96a17
kouki <koukifujii0512@gmail.com>
2024-06-25 15:30:16 +0900

    feat: 回答プログラム完成

--- faq_ai/main.py
+++ faq_ai/main.py
@@ -7,25 +7,25 @@ sheet_name = "sheet1"
 
 io = data.ImportData(excel_path, sheet_name)
 
-i_question = "何を食べたのでしょうか？"
+i_question = "誰が看病してくれたのですか？"
 
 highest_similarity = 0
 most_similar_index = 0
 for idx in range(io.data_index):
+    sentences = []
     for i in range(3):
-        sentences = []
         column_name = "質問" + str(i+1)
         question = io.get_data(idx, column_name)
         if question is not None:
             sentences.append(question)
         else:
             break
-    
     (_, similarity) = SQ.find_most_similar_sentence(sentences, i_question)
-
+    #print(idx, similarity)
     if highest_similarity < similarity:
+        highest_similarity = similarity
         most_similar_index = idx
 
-most_similar_sentence = io.get_data(idx, "原文")
+most_similar_sentence = io.get_data(most_similar_index, "原文")
 
-print(most_similar_index, most_similar_sentence)
+print(most_similar_index + 1, highest_similarity, most_similar_sentence)
