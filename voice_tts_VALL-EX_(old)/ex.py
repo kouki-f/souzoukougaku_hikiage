b757a7d15903dc48ebe4f01d7c4fbfde135d6a5c
clay7614 <167842615+clay7614@users.noreply.github.com>
2024-07-02 13:45:16 +0900

    fix: 不必要なフォルダを除外

--- voice_tts_VALL-EX_(old)/ex.py
+++
@@ -1,8 +0,0 @@
-lang2token = {
-    'zh': "[ZH]",
-    'ja': "[JA]",
-    "en": "[EN]",
-    'mix': "",
-}
-
-print(lang2token['ja'])
--- voice_tts_VALL-EX_(old)/run.py
+++
@@ -1,7 +0,0 @@
-from vclone import tts
-
-input_prompt = "00619_1.mp3"
-input_prompt_text = "三分の一ぐらいの量しか貰えん。とても、そんなもん、腹いっぱいになるようなもんでない。"
-output_prompt_text = "そういうひどい収容所に放り込まれてそれからは次から次とであっちこっち引っ張り回されて農場へ行ったりそれから工場、農場関係の仕事に連れていかれたりね"
-
-tts(input_prompt, input_prompt_text, output_prompt_text)
