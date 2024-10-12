174b3e2a69b4b236da4801fb442d811343ccfed1
clay7614 <167842615+clay7614@users.noreply.github.com>
2024-10-12 09:20:02 +0900

    Revert "update: 英語モデルのロード方法を変更、データベースを更新"
    This reverts commit c3976df0d9d5a9ef0e5f40e92283646097c22538.

--- faq_ai/search_question.py
+++ faq_ai/search_question.py
@@ -1,7 +1,7 @@
 import spacy
 
 # spaCyの英語モデルをロード
-nlp = spacy.load("_internal\ja_core_news_md\ja_core_news_md-3.7.0")
+nlp = spacy.load("ja_core_news_md")
 
 def find_most_similar_sentence(sentences, target_sentence): #sentences=list, target_sentence=str
     # ターゲットの文章をspaCyのドキュメントオブジェクトに変換
